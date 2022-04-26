724cb7f0646f6a6037870a773501fd90b3c987bc
tanmay-raj-29 <tanmayraj29.20@gmail.com>
2022-04-26 23:23:44 +0530

    Add sidebar component

--- src/components/layout/SideBar.js
+++ src/components/layout/SideBar.js
@@ -1,40 +1,25 @@
-import { useState } from 'react';
-import { Button, ButtonGroup } from 'react-bootstrap';
+import { Stack } from 'react-bootstrap';
+import { NavLink } from "react-router-dom";
 import classes from "./SideBar.module.css";
-
-const sideBarItems = [
-    {
-        text: 'Projects',
-        icon: <i className='bx bx-home'></i>,
-        to: '/',
-        section: ''
-    },
-    {
-        text: 'Classroom',
-        icon: <i className='bx bx-star'></i>,
-        to: '/started',
-        section: 'started'
-    },
-    {
-        text: 'Permissions',
-        icon: <i className='bx bx-calendar'></i>,
-        to: '/calendar',
-        section: 'calendar'
-    },
-]
+import { sideBarItems } from "./SideBarItem";
 
 const SideBar = () => {
     return <div className={classes.sidebar}>
-        <ButtonGroup vertical>
-            {
-                sideBarItems.map((item, index) => (<button variant="light">
-                    <div key={index}>
-                        {item.text}
+        <Stack>
+            {sideBarItems.map((item, index) => (
+                <NavLink
+                    key={index}
+                    to={item.to}
+                    className={classes["sidebar-item"]}
+                    activeClassName={classes.active}
+                >
+                    <div className={classes.icon}>
+                        {item.icon}
                     </div>
-                </button>
-                ))
-            }
-        </ButtonGroup>
+                    {item.text}
+                </NavLink>
+            ))}
+        </Stack>
     </div>;
 };
 
